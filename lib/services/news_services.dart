3339f1d4e6c9daae1577c4e5c63ca2aa8e899986
AbdallaSalah200 <abdallasalah3333@gmail.com>
2023-09-02 05:32:51 +0300

    convert list of map to list of object

--- lib/services/news_services.dart
+++ lib/services/news_services.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:news_app_ui_setup/models/artical_model.dart';
 
 class NewsServices {
 final Dio  dio;
@@ -6,7 +7,14 @@ final Dio  dio;
   NewsServices( this.dio);
    getnews()async{
     var response =await dio.get('https://newsapi.org/v2/top-headlines?apiKey=e3356860b2d843a28dae59e708fecd86&country=eg&category=general');
-    print(response);
+    //print(response);
+    Map <String,dynamic> jsonData= response.data ;
+    List <dynamic> articles =jsonData['articles'];
+    List< ArticleModel> articalsList =[];
+    for (var artical in articles) {
+      ArticleModel articalmodel =ArticleModel(image: artical['urlToImage'], title: artical['title'], subTitle: artical['description']);
+      articalsList.add(articalmodel);
+    }
    }
 
 }
