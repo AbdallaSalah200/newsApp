1e5e3bb543644072579b843d0d3836ad512e9f44
AbdallaSalah200 <abdallasalah3333@gmail.com>
2023-09-03 04:50:09 +0300

    create category view

--- lib/widgets/catgory_card.dart
+++ lib/widgets/catgory_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:news_app_ui_setup/Views/Category_view.dart';
 import 'package:news_app_ui_setup/models/category_model.dart';
 
 
@@ -10,20 +11,29 @@ class CategoryCard extends StatelessWidget {
  final CategoryModel categories ;
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(right: 12),
-      child: Container(
-        height: 110,
-        width: 200,
-        
-        decoration: BoxDecoration(
-          image:   DecorationImage(image: AssetImage(categories.image)),
-          borderRadius: BorderRadius.circular(32)
-        ),
-        child:   Center(
-          child:   Text(categories.title,style: const  TextStyle(
-            color: Colors.white,
-          ),),
+    return GestureDetector(
+      onTap: (){
+        Navigator.of(context).push( MaterialPageRoute(
+            builder: (context) {
+              return CategoryView();           
+            },
+          ),);
+      },
+      child: Padding(
+        padding: const EdgeInsets.only(right: 12),
+        child: Container(
+          height: 110,
+          width: 200,
+          
+          decoration: BoxDecoration(
+            image:   DecorationImage(image: AssetImage(categories.image)),
+            borderRadius: BorderRadius.circular(32)
+          ),
+          child:   Center(
+            child:   Text(categories.title,style: const  TextStyle(
+              color: Colors.white,
+            ),),
+          ),
         ),
       ),
     );
